6fa31d7a4dde205a7b99f8189823980a3a41e703
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2022-09-12 16:45:02 -0300

    Estrutura Página Home feita

---
+++ MiniChallenge1/MiniChallenge1/View/PagHome/PaginaHome.swift
@@ -0,0 +1,31 @@
+//
+//  PaginaHome.swift
+//  MiniChallenge1
+//
+//  Created by Pedro Henrique Dias Hemmel de Oliveira Souza on 12/09/22.
+//
+
+import SwiftUI
+
+struct PaginaHome: View {
+    var body: some View {
+        ZStack {
+           
+        }
+    }
+
+}
+
+struct PaginaHome_Previews: PreviewProvider {
+    static var previews: some View {
+        PaginaHome()
+    }
+}
+
+//FUNÇÕES
+
+extension PaginaHome{
+   
+    
+
+}
